29962bac11f34af50dfea0d29a33a0b9c4470aa3
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-12-10 15:45:58 -0600

    set price value on clicking order button

--- app/src/main/java/dev/gopikrishna19/justjava/MainActivity.java
+++ app/src/main/java/dev/gopikrishna19/justjava/MainActivity.java
@@ -1,23 +1,37 @@
 package dev.gopikrishna19.justjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
     public void submitOrder(View view) {
-        display(1);
+
+        int numberOfCoffees = 2;
+
+        display(numberOfCoffees);
+        displayPrice(numberOfCoffees * 5);
+    }
+
+    private void displayPrice(int number) {
+
+        TextView txtPrice = (TextView) findViewById(R.id.txtPrice);
+        txtPrice.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
     private void display(int number) {
+
         TextView txtQuantity = (TextView) findViewById(R.id.txtQuantity);
         txtQuantity.setText("" + number);
     }
