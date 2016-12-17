00beabce8d721d7d838f44b26a71f01710371034
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-12-17 13:17:00 -0600

    loop through words to create and display text views

--- app/src/main/java/dev/gopikrishna19/miwok/NumbersActivity.java
+++ app/src/main/java/dev/gopikrishna19/miwok/NumbersActivity.java
@@ -1,7 +1,12 @@
 package dev.gopikrishna19.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class NumbersActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<>(Arrays.asList("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"));
+
+        LinearLayout numbersActivity = (LinearLayout) findViewById(R.id.activity_numbers);
+
+        for (String word : words) {
+
+            TextView item = new TextView(this);
+            item.setText(word);
+            numbersActivity.addView(item);
+        }
     }
 }
