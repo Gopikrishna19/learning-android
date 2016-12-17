32bbbacc7a1b12c3ddd12b2497d33a7ded135ae6
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-12-17 17:25:42 -0600

    create custom adapter to render the words

---
+++ app/src/main/java/dev/gopikrishna19/miwok/types/WordAdapter.java
@@ -0,0 +1,39 @@
+package dev.gopikrishna19.miwok.types;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import dev.gopikrishna19.miwok.R;
+
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    public WordAdapter(Context context, ArrayList<Word> words) {
+
+        super(context, 0, words);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+
+        View listItem = convertView;
+        if (listItem == null) {
+            listItem = LayoutInflater.from(getContext()).inflate(R.layout.list_item_word, parent, false);
+        }
+
+        Word word = getItem(position);
+        assert word != null;
+
+        ((TextView) listItem.findViewById(R.id.txtMiwok)).setText(word.getTxtMiwok());
+        ((TextView) listItem.findViewById(R.id.txtDefault)).setText(word.getTxtDefault());
+
+        return listItem;
+    }
+}
