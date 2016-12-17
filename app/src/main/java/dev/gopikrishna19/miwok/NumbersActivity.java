6a752f547688d099de13dd63114e24a7757cc2e6
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-12-17 14:11:23 -0600

    use listview and array adapter for better performance

--- app/src/main/java/dev/gopikrishna19/miwok/NumbersActivity.java
+++ app/src/main/java/dev/gopikrishna19/miwok/NumbersActivity.java
@@ -2,8 +2,8 @@ package dev.gopikrishna19.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,14 +17,10 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
         ArrayList<String> words = new ArrayList<>(Arrays.asList("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"));
+        ArrayAdapter<String> wordsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, words);
 
-        LinearLayout numbersActivity = (LinearLayout) findViewById(R.id.activity_numbers);
+        ListView numbersActivity = (ListView) findViewById(R.id.activity_numbers);
 
-        for (String word : words) {
-
-            TextView item = new TextView(this);
-            item.setText(word);
-            numbersActivity.addView(item);
-        }
+        numbersActivity.setAdapter(wordsAdapter);
     }
 }
