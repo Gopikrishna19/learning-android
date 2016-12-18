e78689eaf4ac3a332c9759a3e5600512ed8b9ccf
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-12-17 20:37:17 -0600

    include illustration in the words list and adapter

--- app/src/main/java/dev/gopikrishna19/miwok/types/WordAdapter.java
+++ app/src/main/java/dev/gopikrishna19/miwok/types/WordAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -31,6 +32,7 @@ public class WordAdapter extends ArrayAdapter<Word> {
         Word word = getItem(position);
         assert word != null;
 
+        ((ImageView) listItem.findViewById(R.id.imgIllustration)).setImageResource(word.getImgIllustration());
         ((TextView) listItem.findViewById(R.id.txtMiwok)).setText(word.getTxtMiwok());
         ((TextView) listItem.findViewById(R.id.txtDefault)).setText(word.getTxtDefault());
 
