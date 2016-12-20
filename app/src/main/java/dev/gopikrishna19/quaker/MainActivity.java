f3d835a3a46ddca2ca156987cf7f6d27e3b886e0
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-12-19 19:51:29 -0600

    add simple set of locations

--- app/src/main/java/dev/gopikrishna19/quaker/MainActivity.java
+++ app/src/main/java/dev/gopikrishna19/quaker/MainActivity.java
@@ -1,7 +1,11 @@
 package dev.gopikrishna19.quaker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> locations = new ArrayList<>();
+        locations.add("San Francisco");
+        locations.add("London");
+        locations.add("Tokyo");
+        locations.add("Mexico City");
+        locations.add("Moscow");
+        locations.add("Rio de Janeiro");
+        locations.add("Paris");
+
+        ListView earthquakeListView = (ListView) findViewById(R.id.lvMain);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, locations);
+
+        earthquakeListView.setAdapter(adapter);
     }
 }
