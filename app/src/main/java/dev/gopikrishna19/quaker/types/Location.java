64749ed5abb5b734bb89c5fe02089d86a54961d5
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-12-23 19:01:36 -0600

    migrate code to right context

--- app/src/main/java/dev/gopikrishna19/quaker/types/Location.java
+++ app/src/main/java/dev/gopikrishna19/quaker/types/Location.java
@@ -9,31 +9,16 @@ public class Location {
     private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("0.0");
     private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("MMM dd, yyyy", Locale.getDefault());
     private static final SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("h:mm a", Locale.getDefault());
-    private static final String LOCATION_SEPARATOR = " of ";
-
-    private String name;
-    private String relativeName;
 
     private double magnitude;
+    private String name;
     private long dateTime;
 
     public Location(String name, double magnitude, long dateTime) {
 
         this.magnitude = magnitude;
         this.dateTime = dateTime * 1000L;
-        setName(name);
-    }
-
-    private void setName(String name) {
-
-        if (name.contains(LOCATION_SEPARATOR)) {
-            String[] parts = name.split(LOCATION_SEPARATOR);
-            this.relativeName = parts[0] + LOCATION_SEPARATOR;
-            this.name = parts[1];
-        } else {
-            this.relativeName = "Near the ";
-            this.name = name;
-        }
+        this.name = name;
     }
 
     String getDateString() {
@@ -54,10 +39,5 @@ public class Location {
     String getName() {
 
         return name;
-    }
-
-    String getRelativeName() {
-
-        return relativeName;
     }
 }
--- app/src/main/java/dev/gopikrishna19/quaker/types/LocationAdapter.java
+++ app/src/main/java/dev/gopikrishna19/quaker/types/LocationAdapter.java
@@ -13,10 +13,29 @@ import java.util.ArrayList;
 import dev.gopikrishna19.quaker.R;
 
 public class LocationAdapter extends ArrayAdapter<Location> {
+    private static final String LOCATION_SEPARATOR = " of ";
 
     public LocationAdapter(Context context, ArrayList<Location> locations) {
 
         super(context, 0, locations);
+    }
+
+    private String getRelativeName(String name) {
+
+        if (name.contains(LOCATION_SEPARATOR)) {
+            return name.split(LOCATION_SEPARATOR)[0] + LOCATION_SEPARATOR;
+        }
+
+        return getContext().getString(R.string.near_the);
+    }
+
+    private String getName(String name) {
+
+        if (name.contains(LOCATION_SEPARATOR)) {
+            return name.split(LOCATION_SEPARATOR)[1];
+        }
+
+        return name;
     }
 
     @NonNull
@@ -32,10 +51,12 @@ public class LocationAdapter extends ArrayAdapter<Location> {
         Location location = getItem(position);
         assert location != null;
 
+        String locationName = location.getName();
+
         ((TextView) listItem.findViewById(R.id.txtDate)).setText(location.getDateString());
-        ((TextView) listItem.findViewById(R.id.txtLocation)).setText(location.getName());
+        ((TextView) listItem.findViewById(R.id.txtLocation)).setText(getName(locationName));
         ((TextView) listItem.findViewById(R.id.txtMagnitude)).setText(location.getMagnitude());
-        ((TextView) listItem.findViewById(R.id.txtRelativeLocation)).setText(location.getRelativeName());
+        ((TextView) listItem.findViewById(R.id.txtRelativeLocation)).setText(getRelativeName(locationName));
         ((TextView) listItem.findViewById(R.id.txtTime)).setText(location.getTimeString());
 
         return listItem;
