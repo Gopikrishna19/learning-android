56d4607d485d6940125b0d88cb44ff494bc06ec4
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-12-23 18:40:49 -0600

    create utility for parsing json and populating locations

--- app/src/main/java/dev/gopikrishna19/quaker/MainActivity.java
+++ app/src/main/java/dev/gopikrishna19/quaker/MainActivity.java
@@ -4,11 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import dev.gopikrishna19.quaker.types.Location;
 import dev.gopikrishna19.quaker.types.LocationAdapter;
+import dev.gopikrishna19.quaker.utils.Locations;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,21 +15,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<Location> locations = new ArrayList<>(
-                Arrays.asList(
-                        new Location("San Francisco", 4.5, 1482534399),
-                        new Location("London", 4.5, 1482534399),
-                        new Location("Tokyo", 4.5, 1482534399),
-                        new Location("Mexico City", 4.5, 1482534399),
-                        new Location("Moscow", 4.5, 1482534399),
-                        new Location("Rio de Janeiro", 4.5, 1482534399),
-                        new Location("Paris", 4.5, 1482534399)
-                )
-        );
-
         ListView earthquakeListView = (ListView) findViewById(R.id.lvMain);
 
-        LocationAdapter adapter = new LocationAdapter(this, locations);
+        LocationAdapter adapter = new LocationAdapter(this, Locations.getLocations());
 
         earthquakeListView.setAdapter(adapter);
     }
