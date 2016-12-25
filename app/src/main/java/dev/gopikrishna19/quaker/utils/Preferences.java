770b059d1d88be6f8f0d3beb89c0988048779811
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-12-24 19:20:28 -0600

    show selected value below the preference

--- app/src/main/java/dev/gopikrishna19/quaker/utils/Preferences.java
+++ app/src/main/java/dev/gopikrishna19/quaker/utils/Preferences.java
@@ -1,16 +1,40 @@
 package dev.gopikrishna19.quaker.utils;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 import dev.gopikrishna19.quaker.R;
 
-public class Preferences extends PreferenceFragment {
+public class Preferences extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings_main);
+
+        Preference minMagnitude = findPreference(getString(R.string.min_magnitude_key));
+
+        bindPreferenceSummary(minMagnitude);
+    }
+
+    private void bindPreferenceSummary(Preference preference) {
+
+        preference.setOnPreferenceChangeListener(this);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+        String summary = preferences.getString(preference.getKey(), getString(R.string.min_magnitude_default));
+        preference.setSummary(summary);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+
+        preference.setSummary(value.toString());
+
+        return true;
     }
 }
