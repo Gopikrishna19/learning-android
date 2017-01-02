71059b8ca56379645e09d82eea49f37cfb15a995
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-01-02 17:18:08 -0600

    set gender spinner values

--- app/src/main/java/dev/gopikrishna19/pets/EditorActivity.java
+++ app/src/main/java/dev/gopikrishna19/pets/EditorActivity.java
@@ -2,16 +2,49 @@ package dev.gopikrishna19.pets;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 public class EditorActivity extends AppCompatActivity {
+    private int petGender = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        handleGenderSelect();
+    }
+
+    private void handleGenderSelect() {
+
+        ((Spinner) findViewById(R.id.spinner_gender)).setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+
+                String selection = (String) adapterView.getItemAtPosition(position);
+                if (!TextUtils.isEmpty(selection)) {
+                    if (selection.equals(getString(R.string.gender_male))) {
+                        petGender = 1;
+                    } else if (selection.equals(getString(R.string.gender_female))) {
+                        petGender = 2;
+                    } else {
+                        petGender = 0;
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+                petGender = 0;
+            }
+        });
     }
 
     @Override
