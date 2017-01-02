945557b311d3eb1da734cbf917e1d2308f19750f
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-01-02 13:01:10 -0600

    rename to catalog activity

--- app/src/main/java/dev/gopikrishna19/pets/CatalogActivity.java
+++ app/src/main/java/dev/gopikrishna19/pets/CatalogActivity.java
@@ -3,12 +3,12 @@ package dev.gopikrishna19.pets;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class CatalogActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_catalog);
     }
 }
