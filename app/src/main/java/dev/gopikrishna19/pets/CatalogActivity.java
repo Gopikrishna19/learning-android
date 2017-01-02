f307520387be194db9a07a954a10a2790ab2d1ef
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-01-02 13:51:35 -0600

    set up activity menus

--- app/src/main/java/dev/gopikrishna19/pets/CatalogActivity.java
+++ app/src/main/java/dev/gopikrishna19/pets/CatalogActivity.java
@@ -1,7 +1,11 @@
 package dev.gopikrishna19.pets;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class CatalogActivity extends AppCompatActivity {
 
@@ -10,5 +14,32 @@ public class CatalogActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        handleFabClick();
+    }
+
+    private void handleFabClick() {
+
+        findViewById(R.id.fabNewPet).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/dev/gopikrishna19/pets/EditorActivity.java
+++ app/src/main/java/dev/gopikrishna19/pets/EditorActivity.java
@@ -1,7 +1,9 @@
 package dev.gopikrishna19.pets;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class EditorActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class EditorActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
     }
 }
